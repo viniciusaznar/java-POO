329abf4c82568542022d0671506f84512eec5149
viniciusaznar <140916284+viniciusaznar@users.noreply.github.com>
2023-08-22 11:38:45 -0300

    Add files via upload
    Atualizando aulas

---
+++ Main_Base_Trapezio.java
@@ -0,0 +1,20 @@
+
+public class Main_Base_Trapezio {
+
+	public static void main(String[] args) {
+		
+		// Área do Trapézio
+		
+				double baseMenor, baseMaior, altura, area;
+				
+				baseMenor = 6;
+				baseMaior = 8;
+				altura = 5;
+				
+				area = ((baseMenor + baseMaior) / 2) * altura;
+				
+				System.out.println(area);
+
+	}
+
+}
---
+++ Main_Processamento_Casting.java
@@ -0,0 +1,31 @@
+
+public class Main_Processamento_Casting {
+
+	public static void main(String[] args) {
+
+		int x;
+		double y;
+		
+		x = 5;
+		
+		y = 2 * x;
+		
+		System.out.println(x);
+		System.out.println(y);
+		
+		// Área do Trapézio
+		
+		double baseMenor, baseMaior, altura, area;
+		
+		baseMenor = 6.0;
+		baseMaior = 8.0;
+		altura = 5.0;
+		
+		area = ((baseMenor + baseMaior) / 2) * altura;
+		
+		System.out.println(area);
+		
+		
+	}
+
+}
